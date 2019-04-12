4dfae6408171b23346bdd85cf8f8273cc3da3671
Harold <hhausman@gmail.com>
2019-04-12 11:42:10 -0600

    [js] var let and semicolons.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Variables
-var margin = 5
-var speed = 10
+let margin = 5
+let speed = 10
 
 function LSystem(init, rules, generations) {
-  var out = init
-  for( var i=0; i<generations; i++ ) {
+  let out = init
+  for( let i=0; i<generations; i++ ) {
     out = out.replace(/./g, function(c) {
-      var replacement = rules[c]
+      let replacement = rules[c]
       return replacement ? replacement : c
     })
   }
@@ -15,15 +15,15 @@ function LSystem(init, rules, generations) {
 }
 
 function* Turtle(commands) {
-  var theta = 0;
-  var x = 0;
-  var y = 0;
+  let theta = 0
+  let x = 0
+  let y = 0
   commands = commands.split("")
-  for( var i=0; i<commands.length; i++ ) {
-    var c = commands[i]
+  for( let i=0; i<commands.length; i++ ) {
+    let c = commands[i]
     if( "F" == c ) {
-      var oldX = x
-      var oldY = y
+      let oldX = x
+      let oldY = y
       x += speed * Math.cos(theta)
       y += speed * Math.sin(theta)
       yield [oldX, oldY, x, y]
@@ -38,8 +38,8 @@ function minMax([x1, y1, x2, y2], [lx1, ly1, lx2, ly2]) {
           Math.max(x1, x2, lx1, lx2), Math.max(y1, y2, ly1, ly2)]
 }
 
-var generator;
-var rotationSpeed;
+var generator
+var rotationSpeed
 if( location.href.includes("dragon") ) {
   generator = Turtle(LSystem("FX", {"X": "X+YF+", "Y": "-FX-Y"}, 12))
   rotationSpeed = Math.PI / 2
@@ -48,16 +48,16 @@ if( location.href.includes("dragon") ) {
   rotationSpeed = Math.PI / 3
 }
 
-var data = []
-var viewBoxData = [0, 0, 0, 0]
+let data = []
+let viewBoxData = [0, 0, 0, 0]
 
 function frame() {
-  var v = generator.next().value
+  let v = generator.next().value
   if(v) {
     data.push(v)
     viewBoxData = minMax(v, viewBoxData)
-    var w = viewBoxData[2] - viewBoxData[0]
-    var h = viewBoxData[3] - viewBoxData[1]
+    let w = viewBoxData[2] - viewBoxData[0]
+    let h = viewBoxData[3] - viewBoxData[1]
     viewBox = `${(viewBoxData[0] - margin)} ${(viewBoxData[1] - margin)} ${(w + 2 * margin)} ${(h + 2 * margin)}`
 
     d3.select("#drawing")
